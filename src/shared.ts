6827b94793983ce4d2965b0050a26355950986dd
Blake Knight <bknight@gsandf.com>
2021-12-02 14:30:24 -0600

    :truck: reorganize types

--- src/shared.ts
+++ src/shared.ts
@@ -1,10 +1,59 @@
 import { parse as parsePath } from 'pathington';
+import * as TS from 'ts-toolbelt';
 
 export type PathPart = keyof any;
 export type Path = string | Array<PathPart> | ReadonlyArray<PathPart>;
 
 export type ObjectKey = keyof any;
 export type ObjectLike = Record<ObjectKey, any>;
+
+export type DeepGet<
+  Object,
+  PropertyPath extends Path,
+  Fallback = unknown
+> = PropertyPath extends readonly PathPart[]
+  ? TS.Object.Path<Object, PropertyPath>
+  : PropertyPath extends string
+  ? TS.Object.Path<Object, TS.String.Split<PropertyPath, '.'>>
+  : Fallback;
+
+export type DeepOmit<Object, PropertyPath extends Path> =
+  // convert string path to array
+  PropertyPath extends string
+    ? DeepOmit<Object, TS.String.Split<PropertyPath, '.'>>
+    : // empty path returns original object
+    PropertyPath extends []
+    ? Object
+    : PropertyPath extends [infer Key, ...infer Rest]
+    ? // check if the path's end has been reached
+      Rest extends []
+      ? Key extends keyof Object
+        ? {
+            [Prop in Exclude<keyof Object, Key>]: DeepOmit<Object[Prop], Rest>;
+          }
+        : Object
+      : // check the next part of the path
+      Rest extends PathPart[]
+      ? Key extends keyof Object
+        ? {
+            [Prop in keyof Object]: DeepOmit<Object[Prop], Rest>;
+          }
+        : Object
+      : Object
+    : Object;
+
+export type DeepOmitPaths<
+  Object,
+  PropertyPaths extends Path[]
+> = PropertyPaths extends []
+  ? Object
+  : PropertyPaths extends [infer First, ...infer Rest]
+  ? First extends Path
+    ? Rest extends Path[]
+      ? DeepOmitPaths<DeepOmit<Object, First>, Rest>
+      : never
+    : never
+  : never;
 
 export const NotFound = Symbol('value was not found');
 
